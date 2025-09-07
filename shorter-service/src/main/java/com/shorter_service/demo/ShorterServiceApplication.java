35d8b3721e389fd2330ebdeb31e7a6da4908fbf3
Felipe Laurentino <felipedantaslaurentinodev@gmail.com>
2025-09-07 19:36:09 +0100

    First commit

---
+++ shorter-service/src/main/java/com/shorter_service/demo/ShorterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shorter_service.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShorterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShorterServiceApplication.class, args);
+	}
+
+}
---
+++ shorter-service/src/test/java/com/shorter_service/demo/ShorterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shorter_service.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShorterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
